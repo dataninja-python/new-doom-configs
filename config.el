0de605e437f64b749fd52b60c27c9266e8a11bdc
AJ Igherighe <the.coders.dao@gmail.com>
2022-10-12 18:07:07 -0400

    something broke

--- config.el
+++ config.el
@@ -47,7 +47,8 @@
 ;;  :config
 ;;  (load-theme 'monokai-pro t))
 
-(setq doom-theme 'zenburn)
+;; (setq doom-theme 'zenburn)
+(setq doom-theme 'doom-laserwave-padded-modeline)
 ;;(load-theme 'zenburn t)
 ;; ((use-package! tron-legacy-theme
 ;; 		      :config
